b365cacdf8292617af3610b42066b1404363b872
Mateusz Myalski <mateusz.myalski@gmail.com>
2026-03-12 18:44:35 +0100

    Moved balances below net

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -5,6 +5,6 @@ HASH=$(git rev-parse HEAD)
 
 echo "Building image" $IMAGE_NAME:patch-"${HASH::5}"
 
-docker build -t $IMAGE_NAME:latest -t $IMAGE_NAME:path"$(git rev-parse HEAD:)" .
+docker build -t $IMAGE_NAME:latest -t $IMAGE_NAME:patch-"$(git rev-parse HEAD:)" .
 
 echo "Build complete"
