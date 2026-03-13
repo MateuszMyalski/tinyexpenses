4d09ea4be90b9a52cf921bb4d63e3e8811e55766
Mateusz Myalski <mateusz.myalski@gmail.com>
2026-03-13 16:16:37 +0100

    Added build script

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-IMAGE_NAME=tinyexpenses
+IMAGE_BASE_NAME=tinyexpenses
 HASH=$(git rev-parse HEAD)
+IMAGE_PATCH_NAME=$IMAGE_BASE_NAME:patch-"${HASH::5}"
 
-echo "Building image" $IMAGE_NAME:patch-"${HASH::5}"
+echo "Building image" "$IMAGE_PATCH_NAME"
 
-docker build -t $IMAGE_NAME:latest -t $IMAGE_NAME:patch-"$(git rev-parse HEAD:)" .
+docker build -t $IMAGE_BASE_NAME:latest -t "$IMAGE_PATCH_NAME" .
 
 echo "Build complete"
